62cf94f6e1926481ee953ebf13c2054b05de58ea
Magomed2001 <magomedow01@bk.ru>
2021-10-07 16:59:16 +0300

    create: pircing

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function() {
+    var mixer = mixitup('.portfolio__content');
+    $('.slider__iner').slick({
+        
+    });
+});
+
+ScrollReveal({reset: true}).reveal('.title', {delay: 100});
+
+anime({
+    targets: '.logo',
+    translateX: 3
+  });
