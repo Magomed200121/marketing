0be77b28cae6bcc5223ff286976c6eca31a2a6f3
Magomed2001 <magomedow01@bk.ru>
2021-10-15 14:00:18 +0300

    create: adaptive header

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ $(function() {
     $('.slider__iner').slick({
         
     });
+
 });
 
 ScrollReveal({reset: true}).reveal('.title', {delay: 100});
@@ -10,4 +11,10 @@ ScrollReveal({reset: true}).reveal('.title', {delay: 100});
 anime({
     targets: '.logo',
     translateX: 3
-  });
+});
+
+let menuactive = document.querySelector('.menu__active')
+let menulist = document.querySelector('.menu__list')
+menuactive.addEventListener('click', function(){
+    menulist.classList.toggle('popy')
+})
